e299c67722bbc7d301760538eb7322be30a46ced
Luis Angel Santos <santosangel748@gmail.com>
2022-07-12 15:32:46 -0500

    Creando vistas de home y detail y utilizando la etiqueta url

--- premiosapp/polls/models.py
+++ premiosapp/polls/models.py
@@ -3,17 +3,17 @@ from django.utils import timezone
 import datetime
 
 class Question(models.Model):
-    questiom_text = models.CharField(max_length=200)
+    questiontext = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date_published')
 
     def __str__(self):
-        return self.questiom_text
+        return self.questiontext
     
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choices(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
